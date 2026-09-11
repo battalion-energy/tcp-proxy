3c6cff3da45a225a8812a92e34907b86b4fa14a3
nathan <nslee333@gmail.com>
2026-09-11 16:40:35 -0700

    progress

--- src/main.rs
+++ src/main.rs
@@ -27,11 +27,8 @@ use tracing_subscriber::EnvFilter;
 )]
 struct Cli {
     /// Local address:port to accept client connections (e.g., 127.0.0.1:5001)
-    #[arg(short, long = "listen", value_name = "ADDR:PORT")]
-    listen: SocketAddr,
-    /// Remote target address:port to forward to (e.g., 127.0.0.1:9000)
-    #[arg(short, long = "to", value_name = "ADDR:PORT")]
-    to: SocketAddr,
+    #[arg(short, long = "pro", value_name = "LISTEN=TARGET", value_parser = parse_proxy, required = true)]
+    proxies: Vec<Proxy>,
     /// Max time to establish the outbound connection (humantime, e.g., 2s, 500ms)
     #[arg(short, long = "connect-timeout", default_value = "5s", value_parser = humantime::parse_duration, value_name = "DURATION")]
     connect_timeout: Duration,
@@ -48,35 +45,40 @@ async fn main() -> anyhow::Result<()> {
         .init();
 
     let args = Cli::parse();
-    let listener = TcpListener::bind(args.listen)
-        .await
-        .context("unable to bind listener")?;
 
-    info!(listen = %args.listen, to = %args.to, "listening (Ctrl+C exits immediately)");
+    for proxy in args.proxies {
+        let listener = TcpListener::bind(proxy.listen)
+            .await
+            .context("unable to bind listener")?;
 
-    let to = args.to;
-    let connect_timeout = args.connect_timeout;
+        info!(listen = %proxy.listen, to = %&proxy.target, "listening (Ctrl+C exits immediately)");
 
+        tokio::spawn(accept_connections(
+            listener,
+            proxy.target,
+            args.connect_timeout,
+        ));
+    }
+
+    let _ = signal::ctrl_c().await;
+    info!("Ctrl+C received — exiting immediately");
+    Ok(())
+}
+
+async fn accept_connections(listener: TcpListener, remote: SocketAddr, connect_timeout: Duration) {
     let mut next_conn_id: u64 = 1;
 
     loop {
-        tokio::select! {
-            _ = signal::ctrl_c() => {
-                info!("Ctrl+C received — exiting immediately");
-                return Ok(());
+        match listener.accept().await {
+            Ok((socket, client_addr)) => {
+                let id = next_conn_id;
+                next_conn_id += 1;
+                info!(id = id, client = %client_addr,  "accepted connection");
+                let span =
+                    tracing::info_span!("conn", id = id, client = %client_addr, remote = %remote);
+                tokio::spawn(handle_connection(socket, remote, connect_timeout).instrument(span));
             }
-            res = listener.accept() => {
-                match res {
-                    Ok((socket, client_addr)) => {
-                        let id = next_conn_id;
-                        next_conn_id += 1;
-                        info!(id = id, client = %client_addr, "accepted connection");
-                        let span = tracing::info_span!("conn", id = id, client = %client_addr, remote = %to);
-                        tokio::spawn(handle_connection(socket, to, connect_timeout).instrument(span));
-                    }
-                    Err(e) => warn!(error = %e, "failed to accept connection"),
-                }
-            }
+            Err(e) => warn!(error = %e, "failed to accept connection"),
         }
     }
 }
@@ -115,4 +117,26 @@ async fn handle_connection(
             warn!("session error: {err}");
         }
     }
+}
+
+/// One listener, one proxy
+#[derive(Clone, Debug, Parser)]
+struct Proxy {
+    listen: SocketAddr,
+    target: SocketAddr,
+}
+
+fn parse_proxy(s: &str) -> Result<Proxy, anyhow::Error> {
+    let (listen, target) = s
+        .split_once('=')
+        .context("expected LISTEN=TARGET, e.g 127.0.0.1:5001=127.0.0.1:9000")?;
+
+    Ok(Proxy {
+        listen: listen
+            .parse()
+            .context("bad listen address {listen:?}: {e}")?,
+        target: target
+            .parse()
+            .context("bad target address {target:?}: {e}")?,
+    })
 }
